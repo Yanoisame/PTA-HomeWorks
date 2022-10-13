6dee06fe4c0ba03db21d4c22111215d2f139afaa
ShiroYuki <txong123@gmail.com>
2022-10-13 22:39:05 +0800

    新增

--- 常用函数.py
+++ 常用函数.py
@@ -6,7 +6,7 @@
 #print(f'苹果的价格是{price:.3f}')
 #a = eval(input())
 #print('{:x}'.format(a))
-#.split()分割 默认空格
+#.split()分割 默认空格 maxsplit=(拆分次数)
 #map辅助函数
 #map(目标函数，序列)
 #xxx.count()指定字符串统计
@@ -15,8 +15,13 @@
 #xxx.isalpha 判断是否是常规字符
 #xxx.islower 是否小写字符
 #xxx.isupper 是否是大写
+# xxx.swapcase() 大写转换成小写，小写转换成大写
+# xxx.title() 每个单词首字母大写
+# xxx.just(补位长度，补位内容）l和r左右
+# xxx.center(长度，补位内容) 居中对齐
+# xxx.strip()去除内容 有l和r
 #xxx.isspace 是否有空格
-#序列.replace(需要替换的字符串，"替换内容" 默认替换全部，可以指定替换个数)替换指定内容
+#str.replace(old, new[, max])
 #re.findall(匹配规则，原始文档)
 #xxx.startswith(检查字符串是否是以指定子字符串开头)
 #xxx.endswith(检查字符串是否是以指定子字符串结尾)
