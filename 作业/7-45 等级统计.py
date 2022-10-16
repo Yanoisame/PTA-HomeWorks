cafc0f168c624a75c9ace8dc9b160bd4b1c63488
ShiroYuki <txong123@gmail.com>
2022-10-16 17:00:44 +0800

    修正格式

--- 作业/7-45 等级统计.py
+++ 作业/7-45 等级统计.py
@@ -1,8 +1,7 @@
 a = list(input())
 c = sorted(set(a))
-print(f"""A:{a.count("A")}
-B:{a.count("B")}
-C:{a.count("C")}
-D:{a.count("D")}
-F:{a.count("F")}
-     """)
+print(f"""A: {a.count("A")}
+B: {a.count("B")}
+C: {a.count("C")}
+D: {a.count("D")}
+F: {a.count("F")}""")
