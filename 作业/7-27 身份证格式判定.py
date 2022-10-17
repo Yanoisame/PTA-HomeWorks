be3383a5475961f7e727e327618e8ceca69a1b06
ShiroYuki <txong123@gmail.com>
2022-10-17 22:34:59 +0800

    更新

--- 作业/7-27 身份证格式判定.py
+++ 作业/7-27 身份证格式判定.py
@@ -1,6 +1,7 @@
 a = input()
 ws = a[:17]
 ls = a[-1:]
-if len(a) == 18 and str.isdigit(ws) == True and ls.isalnum()== True:
+if len(a) == 18 and ws.isdigit() == True and ls.isalnum() == True:
     print("True")
-else:print("False")
+else:
+    print("False")
