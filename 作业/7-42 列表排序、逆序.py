233a1ca0f35110c9561afb632fa133671c5d1a23
ShiroYuki <txong123@gmail.com>
2022-10-20 16:18:06 +0800

    新增

---
+++ 作业/7-42 列表排序、逆序.py
@@ -0,0 +1,9 @@
+ni = [12, 3, 48, 6, 79, 63, 89, 7]
+ni.reverse()
+print(f"列表逆序结果为：{ni}")
+up = [12, 3, 48, 6, 79, 63, 89, 7]
+up.sort()
+print(f"列表升序排序结果为：{up}")
+low = [12, 3, 48, 6, 79, 63, 89, 7]
+low.sort(reverse=True)
+print(f"列表降序排序结果为：{low}")
