7709a6280517fa85a640db7168d8cafcecf707d8
ShiroYuki <txong123@gmail.com>
2022-10-31 15:55:32 +0800

    简化代码

--- 作业/7-57 计算天数.py
+++ 作业/7-57 计算天数.py
@@ -1,15 +1,7 @@
 year, month, day = map(int, input().split("/"))
-days = 0
-dic = {'1': 31, '2': 28, '3': 31, '4': 30, '5': 31, '6': 30, '7': 31, '8': 31, '9': 30, '10': 31, '11': 30, '12': 31}
-
+lst = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    dic['2'] = 29
-if int(month) > 1:
-    for obj in dic.keys():
-        if month == int(obj):
-            for i in range(1, int(obj)):
-                days += dic[str(i)]
-    days += day
+    lst[1] = 29
+    print(sum(lst[:month - 1]) + day)
 else:
-    days = day
-print(days)
+    print(sum(lst[:month - 1]) + day)
