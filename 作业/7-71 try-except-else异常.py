dce0b74d6e649979641ad9d190ddaa2b579f31bb
ShiroYuki <txong123@gmail.com>
2022-10-31 18:45:10 +0800

    更新为异常判断形式

--- 作业/7-71 try-except-else异常.py
+++ 作业/7-71 try-except-else异常.py
@@ -1,8 +1,9 @@
-A = eval(input())
-if type(A) is not int:
-    print("值错误，您必须输入数值")
-elif A == 0:
-    print("算术错误，您不能输入0")
-else:
-    print(f"""20除以3的结果是: {20/int(A):.2f}
+try:
+    A = int(input())
+    if A == 0:
+        print("算术错误，您不能输入0")
+    else:
+        print(f"""20除以{A}的结果是: {20 / A:.2f}
 没有出现异常""")
+except:
+    print("值错误，您必须输入数值")
