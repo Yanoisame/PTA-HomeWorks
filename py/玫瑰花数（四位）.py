79458765d42b6799e45e8a8f3716d5dfd53ea8fe
ShiroYuki <txong123@gmail.com>
2022-12-21 12:54:09 +0800

    更新润年判断

--- py/玫瑰花数（四位）.py
+++ py/玫瑰花数（四位）.py
@@ -1,7 +1,7 @@
 numble = int(input())
 num_0 = numble // 1000
-num_1 = numble // 100%10
-num_2 = numble%100//10
+num_1 = numble // 100 % 10
+num_2 = numble % 100//10
 num_3 = numble % 10
 if num_0**4 + num_1**4 + num_2**4 + num_3**4 == numble:
     print("True")
